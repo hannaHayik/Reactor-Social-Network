df78c07307a6d018536ae2a66944c2a9576784c0
Hanna Hayik <79789730+hannaHayik@users.noreply.github.com>
2021-05-08 18:52:54 +0300

    Thread-Per-Client Main
    Thread-Per-Client Main file to run, Uses a thread per connection (even if the user isn't communicating, He owns an active thread in the system)

--- Server/src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
+++ Server/src/main/java/bgu/spl/net/impl/BGSServer/TPCMain.java
@@ -6,7 +6,7 @@ import bgu.spl.net.api.bidi.impl.bidiMessageProtocolImpl;
 import bgu.spl.net.srv.Server;
 
 public class TPCMain {
-
+ 
 	public static void main(String[] args) {
 		DataBase db = new DataBase();
 		Server.threadPerClient(Integer.parseInt(args[0]), () -> new bidiMessageProtocolImpl(db), MessageEncoderDecoderImpl::new).serve();
